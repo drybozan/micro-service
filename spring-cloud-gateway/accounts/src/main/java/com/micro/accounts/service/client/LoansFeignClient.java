0c5c45679696f906a7d36d0d135cd1dc61eee283
derya bozan <deryabozan@atmaca.com.tr>
2025-03-06 17:08:51 +0300

    spring cloud gateway sunucusu eklendi.

---
+++ spring-cloud-gateway/accounts/src/main/java/com/micro/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,17 @@
+package com.micro.accounts.service.client;
+
+
+
+import com.micro.accounts.dto.LoansDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("loans")
+public interface LoansFeignClient {
+
+    @GetMapping(value = "/api/fetch",consumes = "application/json")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam String mobileNumber);
+
+}
